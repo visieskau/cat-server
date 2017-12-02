5e59644020c72319f5ff5eebb0ef9a7f416a8865
visieskau <kar.modzelewska@gmail.com>
2017-12-02 21:00:56 +0100

    Zadanie 2
    Flask zawiera mechanizm template. Służy on do tego, żeby nie trzeba było hardkodowac w kodzie python HTMLa. Zadanie polega na przejściu z zahardkodowanego HTML z punktu 1 do template.

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/status')
 def zahardko_status():
-    return 'Działam'
+    return render_template('dzialam.html')
 
 if __name__ == "__main__":
     app.run()
