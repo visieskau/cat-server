00e53b64319f87cbfa292a9e7fc00b6e12b1aef9
visieskau <kar.modzelewska@gmail.com>
2017-12-04 20:01:11 +0100

    Poprawa nazwy funkcji
    "zahardko_status" -> "status"

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/status')
-def zahardko_status():
+def status():
     return render_template('dzialam.html')
 
 if __name__ == "__main__":
