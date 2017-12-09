6bcbb39023d5a47f161a2454966caaf5814bf72b
visieskau <kar.modzelewska@gmail.com>
2017-12-09 22:01:12 +0100

    Zadanie 7
    Tworzymy własną klasę. Klasa niech nazywa się Cat. Cat powinien mieć konstruktor który przyjmuje imię i wiek. Cat powinien mieć funkcję greet() która zwróci np ‘Hello, my name is Connie. I am 4 years old.’

---
+++ animals/__init__.py
@@ -0,0 +1,7 @@
+class Cat:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def greet(self):
+        return "Hello, my name is " + self.name + ". I am " + self.age + " years old."
--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, Response
+from animals import Cat
 app = Flask(__name__)
 
 
