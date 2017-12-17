af5bc4c15ae9581febc9ea03b2f1577212e46ac0
visieskau <kar.modzelewska@gmail.com>
2017-12-17 20:16:51 +0100

    Zadanie 8.1
    Testy jednostkowe dla drugiej metody klasy Cat

--- animals/__init__.py
+++ animals/__init__.py
@@ -5,3 +5,13 @@ class Cat:
 
     def greet(self):
         return "Hello, my name is " + self.name + ". I am " + str(self.age) + " years old."
+
+    def check_oldness(self):
+        if self.age < 1:
+            return self.name + " is a little cat."
+        elif 1 <= self.age < 2:
+            return self.name + " is a young cat."
+        else:
+            return self.name + " is an old cat."
+
+
--- tests/cat_test.py
+++ tests/cat_test.py
@@ -10,3 +10,15 @@ class TestCatMethods(unittest.TestCase):
     def test_greet2(self):
         cat = Cat(name="Panienka", age=2.5)
         self.assertEqual(cat.greet(), "Hello, my name is Panienka. I am 2.5 years old.")
+
+    def test_check_little(self):
+        cat = Cat(name="Bulcyngier", age=0.5)
+        self.assertEqual(cat.check_oldness(), "Bulcyngier is a little cat.")
+
+    def test_check_young(self):
+        cat = Cat (name="Rudy", age=1)
+        self.assertEqual(cat.check_oldness(), "Rudy is a young cat.")
+
+    def test_check_old(self):
+        cat = Cat (name="Choco", age=4)
+        self.assertEqual(cat.check_oldness(), "Choco is an old cat.")
